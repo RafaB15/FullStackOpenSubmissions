09ea33da8e0924dbb0a3cfe8a3a0c8d555376d5b
Rafael Berenguel <rafaelberenguel15@gmail.com>
2023-11-20 23:01:22 -0300

    Finished 1.13

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -15,15 +15,24 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
   ]
+
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
    
   const [selected, setSelected] = useState(0)
 
-  const handleClick = () => setSelected(Math.floor(Math.random() * anecdotes.length))
+  const handleNextAnecdote = () => setSelected(Math.floor(Math.random() * anecdotes.length))
+  const handleVote = () => {
+    const copy = [...votes]
+    copy[selected] += 1
+    setVotes(copy)
+  }
 
   return (
     <div>
       {anecdotes[selected]}<br/>
-      <Button onClicked={handleClick} text='next anecdote'/>
+      has {votes[selected]} votes<br/>
+      <Button onClicked={handleVote} text='vote'/>
+      <Button onClicked={handleNextAnecdote} text='next anecdote'/>
     </div>
   )
 }
