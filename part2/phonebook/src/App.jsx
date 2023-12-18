f8539e728be1ebd42166a784a5952b2326e1a328
Rafael Berenguel <rafaelberenguel15@gmail.com>
2023-12-18 19:34:57 -0300

    Finished 2.15

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -29,8 +29,17 @@ const App = () => {
   const addPerson = (event) => {
     event.preventDefault()
     if (persons.find(person => person.name === newName)) {
-      alert(`${newName} is already added to phonebook`)
+      if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)){
+        const person = persons.find(person => person.name === newName)
+        const changedPerson = { ...person, number: newNumber}
+        personService
+          .update(person.id, changedPerson)
+          .then(returnedPerson => {
+            setPersons(persons.map(person => person.id !== returnedPerson.id ? person : returnedPerson))
+          })
+      }
       setNewName('')
+      setNumber('')
       return
     }
     const personObject = {
