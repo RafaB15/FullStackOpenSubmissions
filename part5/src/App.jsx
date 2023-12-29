71758f87a4703639813089a47e58f5885e23b478
Rafael Berenguel <rafaelberenguel15@gmail.com>
2023-12-29 17:35:25 -0300

    Finished 5.1

---
+++ part5/src/App.jsx
@@ -0,0 +1,78 @@
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import Notification from './components/Notification'
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+  const [notification, setNotification] = useState(null)
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )  
+  }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    
+    try {
+      const userData = await loginService.login({
+        username, password,
+      })
+      setUser(userData)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setNotification('Wrong credentials')
+      setTimeout(() => {
+        setNotification(null)
+      }, 5000)
+    }
+  }
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input 
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input 
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  )
+
+  const blogsForm = () => (
+    blogs.map(blog =>
+      <Blog key={blog.id} blog={blog} />
+    )
+  )
+
+  return (
+    <div>
+      <h2>Blogs</h2>
+      <Notification message = {notification}/>
+      {user === null && loginForm()}
+      {user !== null && blogsForm()}
+    </div>
+  )
+}
+
+export default App
