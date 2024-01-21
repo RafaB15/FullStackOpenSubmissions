838a33ef7ad99f6937de5c8aa3e0730511e2cda4
Rafael Berenguel <rafaelberenguel15@gmail.com>
2024-01-20 23:46:35 -0300

    Finished 6.12

---
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,12 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const notificationSlice = createSlice({
+    name: 'notification',
+    initialState: '',
+    reducers: {
+        createNotification: (state, action) => action.payload,
+    }
+})
+
+export const { createNotification } = notificationSlice.actions
+export default notificationSlice.reducer
